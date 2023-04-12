8108cd7a3316eb1879225f04573c90ada7d2778a
Simon507 <Evgeny.klysak@gmail.com>
2023-04-12 22:22:19 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ export const App = () => {
     <b>Refreshing user...</b>
   ) : (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/goit-react-hw-08-phonebook" element={<Layout />}>
         <Route
           path="/register"
           element={
